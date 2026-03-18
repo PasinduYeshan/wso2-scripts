4b2d2f2bb6e0f159003bf684bda7b08bbc143e94
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2026-03-18 15:40:42 +0530

    Migration scripts

--- users/delete_is_users.sh
+++ users/delete_is_users.sh
@@ -7,11 +7,14 @@ SUPER_BASIC_AUTH="YWRtaW46YWRtaW4="
 WSO2_TENANT_BASIC_AUTH="a2ltOmtpbTEyMw==" # kim kim123
 SUB_BASIC_AUTH="YWRtaW5AMTAwODRhOGQtMTEzZi00MjExLWEwZDUtZWZlMzZiMDgyMjExOmFkbWlu"
 
-ORG_PATH=$WSO2_TENANT_PATH
-BASIC_AUTH=$WSO2_TENANT_BASIC_AUTH
+ORG_PATH=$SUPER_ORG_PATH
+BASIC_AUTH=$SUPER_BASIC_AUTH
 
-USERS_URL="https://localhost:9443/$ORG_PATH/scim2/Users"
-DELETE_URL="https://localhost:9443/$ORG_PATH/scim2/Bulk"
+# Accept optional port as first argument, default to 9443
+PORT=${1:-9443}
+
+USERS_URL="https://localhost:$PORT/$ORG_PATH/scim2/Users"
+DELETE_URL="https://localhost:$PORT/$ORG_PATH/scim2/Bulk"
 
 # Define an array of usernames to ignore.
 IGNORE_USERS=("admin" "admin1" "kim")
@@ -20,18 +23,23 @@ IGNORE_USERS=("admin" "admin1" "kim")
 JQ_IGNORE_USERS=$(printf ",\"%s\"" "${IGNORE_USERS[@]}")
 JQ_IGNORE_USERS=[${JQ_IGNORE_USERS:1}]
 
+TOTAL_DELETED=0
+
 while true; do
     # Fetch the users
     RESPONSE=$(curl -k --location "$USERS_URL" --header "Authorization: Basic $BASIC_AUTH")
 
     # Extract all user IDs where the user name is not in the ignore list
     USER_IDS=$(echo $RESPONSE | jq -r --argjson ignore "$JQ_IGNORE_USERS" '.Resources[] | select(.userName as $u | $ignore | index($u) | not) | .id')
-   
+
     # Check if there are no more users
     if [[ -z "$USER_IDS" || "$USER_IDS" == "null" ]]; then
-        echo "No more users left."
+        echo "No more users left. Total deleted: $TOTAL_DELETED"
         break
     fi
+
+    USER_COUNT=$(echo "$USER_IDS" | wc -l | tr -d ' ')
+    echo "Found $USER_COUNT user(s) to delete..."
 
     # Construct the bulk delete operations array from the user IDs
     DELETE_OPERATIONS=""
@@ -59,6 +67,9 @@ EOL
          --header "Authorization: Basic $BASIC_AUTH" \
          --data "$DELETE_PAYLOAD"
          
+    TOTAL_DELETED=$((TOTAL_DELETED + USER_COUNT))
+    echo "Deleted $USER_COUNT user(s). Total so far: $TOTAL_DELETED"
+
     # Sleep for a short duration to avoid hammering the server
     sleep 2
 
